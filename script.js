51e35b5d3ccfacf0af97b3ad63eb365679fddaf5
Gajendra Kumar Arya <gajendra.work538@gmail.com>
2025-01-02 22:23:04 +0530

    added new files

---
+++ script.js
@@ -0,0 +1,30 @@
+let resume = document.querySelector(".Resume");
+let project = document.querySelector(".Project");
+let contact = document.querySelector(".Contact");
+let innerbody = document.querySelector(".innerbody");
+resume.addEventListener("click", loadPage);
+
+function loadPage() {
+    fetch("ind.html") // Replace with the path to your HTML file
+      .then(response => response.text())
+      .then(data => {
+       innerbody.innerHTML = data;
+      })
+      .catch(error => console.error("Error loading page:", error));
+  }
+
+  // to add external html and cssfile
+  function loadPage2() {
+    fetch("page2.html")
+      .then(response => response.text())
+      .then(data => {
+        document.getElementById("content").innerHTML = data;
+
+        // Load CSS dynamically
+        const link = document.createElement("link");
+        link.rel = "stylesheet";
+        link.href = "styles.css"; // External CSS file
+        document.head.appendChild(link);
+      })
+      .catch(error => console.error("Error loading page:", error));
+  }
