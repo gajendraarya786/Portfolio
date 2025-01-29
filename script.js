ea875953d68a6f6e57463eb3be4cb97b77d6ce81
Gajendra Kumar Arya <gajendra.work538@gmail.com>
2025-01-29 22:33:38 +0530

    final commit

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ let contact = document.querySelector(".Contact");
 let innerbody = document.querySelector(".innerbody");
 let githubBtn = document.querySelector(".btn2");
 let linkedBtn = document.querySelector("#linkedInBtn");
-resume.addEventListener("click", loadPage);
 
+resume.addEventListener("click", loadPage);
 project.addEventListener("click", projectpage);
 contact.addEventListener("click", contactpage);
 
@@ -15,13 +15,20 @@ function redirecttoLinkedIn () {
   window.location.href = "https://www.linkedin.com/in/gajendra-kumar-arya-006076297?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app";
 }
 
-githubBtn.addEventListener("click", redirecttoGithub());
+githubBtn.addEventListener("click", redirecttoGithub);
 
 function redirecttoGithub () {
   window.location.href = "https://github.com/gajendraarya786";
 }
 
+window.addEventListener("popstate", function (event) {
+  if (event.state) {
+      loadPage(event.state.path);
+  }
+});
+
 function loadPage() {
+  history.pushState({ path: "index.html" }, "", "index.html");
     fetch("resume.html") // Replace with the path to your HTML file
       .then(response => response.text())
       .then(data => {
@@ -49,22 +56,6 @@ function loadPage() {
       .catch(error => console.error("Error loading page:", error));
   }
 
-
-  // to add external html and cssfile
-  function loadPage2() {
-    fetch("page2.html")
-      .then(response => response.text())
-      .then(data => {
-        document.getElementById("content").innerHTML = data;
-
-        // Load CSS dynamically
-        const link = document.createElement("link");
-        link.rel = "stylesheet";
-        link.href = "styles.css"; // External CSS file
-        document.head.appendChild(link);
-      })
-      .catch(error => console.error("Error loading page:", error));
-  }
 
   setTimeout(() => {
     resume.classList.add('loaded');
